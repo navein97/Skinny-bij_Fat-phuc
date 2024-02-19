6b9ac8d7a0f802e7bb648baaa92303542b6e1945
navein97 <naveinthiran97@gmail.com>
2024-02-19 20:51:16 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,72 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img v-if="showImage1" src="./assets/fat_phuc.jpeg" alt="Image 1" />
+  <img v-else src="./assets/skinny_bij.jpeg" alt="Image 2" />
+  <h2>{{ name ? "Fat phuc" : "Skinny bij" }}</h2>
+  <button @click="fatty">Click me bastard</button>
+  <HelloWorld />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      name: true,
+      showImage1: true,
+    };
+  },
+  methods: {
+    fatty() {
+      this.name = !this.name;
+      this.showImage1 = !this.showImage1;
+    },
+  },
+};
 </script>
 
 <style>
+
+button {
+  background: linear-gradient(
+    to right,
+    #f4511e,
+    #e91e63,
+    #9c27b0,
+    #673ab7,
+    #3f51b5
+  ); /* Slightly darker hues on hover */
+    border: none;
+    color: white;
+    padding: 15px 32px; 
+    text-align: center;
+    font-size: 16px;
+    cursor: pointer; 
+
+    /* Optional Styling */
+    border-radius: 5px;  /* Add rounded corners */ 
+    box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.2); /* Subtle shadow */
+}
+
+button:hover {
+  background: linear-gradient(
+    to right,
+    #972708,
+    #910a37,
+    #650979,
+    #3f1092,
+    #162b95
+  ); /* Slightly darker hues on hover */
+}
+
+img {
+  max-width: 100%; /* Occupy maximum available width */
+  max-height: 400px; /* Set a maximum height */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
